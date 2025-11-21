7245fc81b8e248b1f2595658388a78b798dfd64a
Persfone <persfone240@gmail.com>
2025-11-21 18:35:15 -0300

    I1, clases Colectivo, Tarjeta y Boleto

---
+++ TarjetaSube/Boleto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TarjetaSube
+{
+     public class Boleto
+    {
+        public string Linea { get; }
+        public double SaldoRestante { get; }
+
+        public Boleto(string linea, double saldoRestante)
+        {
+            Linea = linea;
+            SaldoRestante = saldoRestante;
+        }
+
+        public void Imprimir()
+        {
+            Console.WriteLine($"Boleto emitido para la línea: {Linea}");
+            Console.WriteLine($"Saldo restante en la tarjeta: {SaldoRestante}");
+        }
+    }
+}
---
+++ TarjetaSube/Tarjeta.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;  
+
+public class Tarjeta
+{
+    private double saldo = 0;
+
+
+    private static readonly double[] montosValidos = { 2000, 3000, 4000, 5000, 8000, 10000, 15000, 20000, 25000, 30000 };
+
+    private const double LIMITE_SALDO = 40000;
+
+    public double Saldo
+    {
+        get { return saldo; }
+    }
+
+    public bool Cargar(double monto)
+    {
+   
+        if (!montosValidos.Contains(monto))
+            return false;
+
+        if (saldo + monto > LIMITE_SALDO)
+            return false;
+
+        saldo += monto;
+        return true;
+    }
+
+
+    internal bool Pagar(double monto)
+    {
+        if (saldo >= monto)
+        {
+            saldo -= monto;
+            return true;
+        }
+        return false;
+    }
+}
